601dd7bfb2ad24fbb56265689ed8f2b89451c780
Hüseyin Taha Adanur <tahaadanur61@gmail.com>
2026-02-11 19:38:00 +0300

    Lesson 08: feat: implement N-Tier Architecture and Entity Layer
    Implementing a 4-layer architecture (Business, DataAccess, Entity, Presentation) for scalable applications.

---
+++ PrepUnity301/PrepUnity301.EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepUnity301.EntityLayer.Concrete
+{
+    internal class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerDistrict { get; set; }s
+        public string CustomerCity { get; set; }
+    }
+}
