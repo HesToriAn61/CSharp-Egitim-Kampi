0b21644518fa4f300cf7abf2a57f023834630736
Hüseyin Taha Adanur <tahaadanur61@gmail.com>
2026-02-20 14:42:57 +0300

    Lesson 10: feat: implement generic repository interfaces and setup migrations
    Created IGenericDal<T> to abstract fundamental CRUD operations. Added specific interfaces for entities (IAdminDal, ICategoryDal, etc.) in the DataAccessLayer. Enabled EF migrations for database tracking.

---
+++ PrepUnity301/PrepUnity301.DataAccessLayer/Abstract/IOrderDal.cs
@@ -0,0 +1,14 @@
+﻿using PrepUnity301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepUnity301.DataAccessLayer.Abstract
+{
+    public interface IOrderDal:IGenericDal<Order>
+    {
+       
+    }
+}
